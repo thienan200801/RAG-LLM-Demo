77585266e6c04fbae589e07dca23f0453e4d9b78
Andie.Tran <andie.tran@fmgsuite.com>
2024-11-23 16:22:30 +0700

    feature: remove redundancy

--- compare_embeddings.py
+++ compare_embeddings.py
@@ -4,21 +4,15 @@ from dotenv import load_dotenv
 import openai
 import os
 
-# Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
-#---- Set OpenAI API key 
-# Change environment variable name from "OPENAI_API_KEY" to the name given in 
-# your .env file.
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 def main():
-    # Get embedding for a word.
     embedding_function = OpenAIEmbeddings()
     vector = embedding_function.embed_query("apple")
     print(f"Vector for 'apple': {vector}")
     print(f"Vector length: {len(vector)}")
 
-    # Compare vector of two words
     evaluator = load_evaluator("pairwise_embedding_distance")
     words = ("apple", "iphone")
     x = evaluator.evaluate_string_pairs(prediction=words[0], prediction_b=words[1])
